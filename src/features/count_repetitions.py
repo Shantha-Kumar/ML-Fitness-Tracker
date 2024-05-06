b01a3b86817b573d4c66c0bd3a67f0815ce36560
Shantha Kumar <mailtoshan265@gmail.com>
2024-05-06 15:23:51 +0530

    Experiment with feature selection, model selection and hyperparameter tuning with grid search to find the combination that results in the highest classification accuracy...

---
+++ src/features/count_repetitions.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from src.features.DataTransformation import LowPassFilter
+from scipy.signal import argrelextrema
+from sklearn.metrics import mean_absolute_error
+
+pd.options.mode.chained_assignment = None
+
+
+# Plot settings
+plt.style.use("fivethirtyeight")
+plt.rcParams["figure.figsize"] = (20, 5)
+plt.rcParams["figure.dpi"] = 100
+plt.rcParams["lines.linewidth"] = 2
+
+
+# --------------------------------------------------------------
+# Load data
+# --------------------------------------------------------------
+df = pd.read_pickle("../../data/interim/01_data_processed.pkl")
+df = df[df["label"] != "rest"]
+
+acc_r = df["acc_x"] ** 2 + df["acc_y"] ** 2 + df["acc_z"] ** 2
+gyr_r = df["gyr_x"] ** 2 + df["gyr_y"] ** 2 + df["gyr_z"] ** 2
+
+df["acc_r"] = np.sqrt(acc_r)
+df["gyr_r"] = np.sqrt(gyr_r)
+
+# --------------------------------------------------------------
+# Split data
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Visualize data to identify patterns
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Configure LowPassFilter
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Apply and tweak LowPassFilter
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Create function to count repetitions
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Create benchmark dataframe
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Evaluate the results
+# --------------------------------------------------------------
